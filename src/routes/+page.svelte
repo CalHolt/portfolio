c9dd2e9b17f8abb668d1aaba2ce7fb37ee38feb4
drib <calumholt@hotmail.com>
2023-12-21 11:39:06 -0700

    added basic styling to landing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,13 @@
-<div>
+<div class="flex justify-center h-screen content-center gap-20">
+<div class="flex flex-col justify-center max-w-80">
   <h1 class="text-5xl">Cal Holt</h1>
   <p class="text-xl">Hi my name is Cal, I am a <span class="text-3xl">Junior Web Developer</span> with a passion for front end development, rock climbing and video games :)</p>
 </div>
-<div>
-  <ul>
+<div class="flex flex-col justify-center">
+  <ul class="text-8xl">
     <li><a href="/">Work</a></li>
     <li><a href="/">About</a></li>
     <li><a href="/">Contact</a></li>
   </ul>
+</div>
 </div>
