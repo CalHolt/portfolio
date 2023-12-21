05a4ea992e910598aa9960d7e96b2e95474fb295
drib <calumholt@hotmail.com>
2023-12-21 12:21:36 -0700

    added more styling to landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,26 @@
-<div class="flex justify-center h-screen content-center gap-20">
+<div class="flex justify-center h-screen content-center gap-20 p-5">
 <div class="flex flex-col justify-center max-w-80">
-  <h1 class="text-5xl">Cal Holt</h1>
+  <h1 class="text-5xl my-5">Cal Holt</h1>
   <p class="text-xl">Hi my name is Cal, I am a <span class="text-3xl">Junior Web Developer</span> with a passion for front end development, rock climbing and video games :)</p>
 </div>
 <div class="flex flex-col justify-center">
-  <ul class="text-8xl">
-    <li><a href="/">Work</a></li>
-    <li><a href="/">About</a></li>
-    <li><a href="/">Contact</a></li>
+  <ul class="text-9xl">
+    <li class="my-10"><a href="/">Work</a></li>
+    <li class="my-10"><a href="/">About</a></li>
+    <li class="my-10"><a href="/">Contact</a></li>
   </ul>
 </div>
 </div>
+<style>
+  li {
+    font-family: 'Fugaz One', sans-serif;
+  }
+  p {
+    font-family: 'work sans';
+  }
+  a:hover {
+    text-decoration: 5px solid underline;
+    text-decoration-color: black;
+    transition: ease-in .5s;
+  }
+</style>
