2a9a2069c71182a041c87edfdd373e85454561f1
drib <calumholt@hotmail.com>
2023-12-21 11:10:33 -0700

    basic info added to landing page, no styling added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div>
+  <h1 class="text-5xl">Cal Holt</h1>
+  <p class="text-xl">Hi my name is Cal, I am a <span class="text-3xl">Junior Web Developer</span> with a passion for front end development, rock climbing and video games :)</p>
+</div>
+<div>
+  <ul>
+    <li><a href="/">Work</a></li>
+    <li><a href="/">About</a></li>
+    <li><a href="/">Contact</a></li>
+  </ul>
+</div>
