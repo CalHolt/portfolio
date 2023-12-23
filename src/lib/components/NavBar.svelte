fd11053e5539b2107aada17fb48409d86c860265
drib <calumholt@hotmail.com>
2023-12-22 22:13:03 -0700

    about page in progress

--- src/lib/components/NavBar.svelte
+++ src/lib/components/NavBar.svelte
@@ -1,12 +1,12 @@
-<div class="flex justify-between p-3 items-center sticky m-3 rounded-sm border-black border-solid border-2">
+<div class="flex justify-between p-3 items-center sticky top-4 m-3 rounded-sm border-black border-solid border-2">
   <div>
     <span class="text-3xl"><a href="/">Cal Holt</a></span>
   </div>
   <div>
-    <ul class="flex gap-5">
-      <li><a href="/work">Work</a></li>
-      <li><a href="/about">About</a></li>
-      <li><a href="/contact">Contact</a></li>
+    <ul class="flex gap-5 items-center">
+      <li class="text-xl "><a href="/work">Work</a></li>
+      <li class="text-xl "><a href="/about">About</a></li>
+      <li class="text-xl "><a href="/contact">Contact</a></li>
     </ul>
   </div>
 </div>
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,4 +1,59 @@
 <script>
   import NavBar from "../../lib/components/NavBar.svelte";
+  import SocialBar from "../../lib/components/SocialBar.svelte";
 </script>
 <NavBar />
+<h1 class="text-center text-5xl">-About-</h1>
+<div class="sm:flex-row  flex flex-col justify-between items-center py-40 p-5 sm:px-40 lg:px-80">
+  <div>
+    IMG
+  </div>
+  <div id="card" class="w-40">
+    <p>Hi I am Cal, a newly graduated web developer from SAIT with a passion for front end development. On this page you can see my resume, languages / tools I know and a bit about me personally.</p>
+  </div>
+</div>
+<div class="sm:flex-row gap-5 flex flex-col justify-between items-center p-5 sm:px-40 lg:px-80 shadow shadow-2xl">
+ <div class="align-center ">
+    <h2 class="text-3xl">Languages and Tools</h2>
+ </div>
+ <div class="flex  gap-10 text-center">
+  <div>
+    <ul>
+      <li>HTML</li>
+      <li>CSS</li>
+      <li>JavaScript</li>
+      <li>Tailwind CSS</li>
+      <li>SvelteKit</li>
+      <li>SkeletonUI</li>
+    </ul>
+  </div>
+ <div>
+  <ul>  
+    <li>Directus</li>
+    <li>Supabase</li>
+    <li>Netlify / Vercel</li>
+    <li>Figma / Figjam</li>
+    <li>Git</li>
+    <li>NPM</li>
+  </ul>
+ </div>
+  
+ </div> 
+</div>
+
+<div>
+  <div>
+    <span>Music I Love</span>
+  </div>
+<div>
+  <iframe title="spotify" style="border-radius:12px" src="https://open.spotify.com/embed/playlist/5GSE3gz9ZGDesTJw8hMSUw?utm_source=generator&theme=0" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+</div>
+</div>
+<footer class="bg-black p-5 flex content-center items-center">
+  <SocialBar />
+</footer>
+<style>
+  h1, h2 {
+    font-family: 'Fugaz One', sans-serif;
+  }
+</style>
