4339bdc15b74e4e2edc082120b74ea643fb0d3da
drib <calumholt@hotmail.com>
2023-12-29 09:21:08 -0700

    changes

---
+++ src/lib/components/Card.svelte
@@ -0,0 +1,29 @@
+<script>
+  export let cardTitle;
+  export let item1;
+  export let item2;
+  export let item3; 
+</script>
+<div class="card">
+  <h1 class="text-xl">{cardTitle}</h1>
+  <ul>
+    <li>{item1}</li>
+    <li>{item2}</li>
+    <li>{item3}</li>
+  </ul>
+</div>
+<style>
+  h1 {
+    font-family: 'Fugaz One', sans-serif;
+    margin-bottom: 10px;
+    text-decoration: underline;
+  }
+  li {
+    font-family: 'work sans';
+  }
+  .card {
+    padding: 2rem;
+    box-shadow: 10px 10px #000000;
+    border: 2px solid #000000;
+  }
+</style>
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -2,13 +2,27 @@
   import NavBar from "../../lib/components/NavBar.svelte";
   import SocialBar from "../../lib/components/SocialBar.svelte";
   import DownloadButton from "../../lib/components/DownloadButton.svelte";
+  import Card from "../../lib/components/Card.svelte";
+  let myInfo = [
+    {cardTitle: "Games I'm Playing",
+    item1: "Rainbow Six Siege",
+    item2: "Mortal Kombat 1",
+    item3: "Chess"},
+    {cardTitle: "Languages I'm Learning",
+    item1: "React",
+    item2: "NextJS",
+    item3: "Angular"},
+    {cardTitle: "Songs On Repeat",
+    item1: "- Im Home by Peter Cat Recording Co.",
+    item2: "- Shiela by Jamie T",
+    item3: "- Dunno by Mac Miller"}]
 </script>
 <NavBar />
 <h1 class="text-center text-5xl">-About-</h1>
 <div class=" flex p-5 justify-center">
   <DownloadButton />
 </div>
-<div class="sm:flex-row bg-black text-white m-3 flex flex-col justify-between items-center gap-10 py-10 sm:py-20 sm:px-40 lg:px-80">
+<div class="sm:flex-row bg-black text-white m-3 flex flex-col justify-between items-center p-5 gap-10 py-10 sm:py-20 sm:px-40 lg:px-80">
   <div>
     <img class="hero" src="../images/myimage.jpeg" alt="me">
   </div>
@@ -43,6 +57,12 @@
   </ul>
  </div>
  </div> 
+</div>
+<h2 class="text-3xl text-center py-5">About Me</h2>
+<div class="sm:flex-row gap-5 flex flex-col justify-center items-center p-5">
+  {#each myInfo as info}
+  <Card {...info}/> 
+ {/each}
 </div>
 
 <footer class="bg-black p-5 flex content-center items-center">
